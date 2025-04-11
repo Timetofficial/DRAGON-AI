9e18abe95fdf3d3916f64467595c49ca990c4547
Timetofficial <166425430+Timetofficial@users.noreply.github.com>
2025-04-11 14:02:58 +0300

    Update set.js

--- set.js
+++ set.js
@@ -4,40 +4,40 @@ There's no need to configure this if you're deploying via Heroku — just set th
 
 const sessionName = 'session';
 const session = process.env.SESSION || '';
-const autobio = process.env.AUTOBIO || 'TRUE';
+const autobio = process.env.AUTOBIO || 'FALSE';
 const autolike = process.env.AUTOLIKE_STATUS || 'TRUE';
 const autoviewstatus = process.env.AUTOVIEW_STATUS || 'TRUE';
 const welcomegoodbye = process.env.WELCOMEGOODBYE || 'FALSE';
 
-const prefix = process.env.PREFIX || '.';
+const prefix = process.env.PREFIX || '';
 const appname = process.env.APP_NAME || '';
 const herokuapi = process.env.HEROKU_API;
 const gptdm = process.env.GPT_INBOX || 'FALSE';
 const mode = process.env.MODE || 'PUBLIC';
 const anticall = process.env.AUTOREJECT_CALL || 'TRUE';
 
-const botname = process.env.BOTNAME || 'DRAGON-AI';
+const botname = process.env.BOTNAME || 'MR HEISME';
 const antibot = process.env.ANTIBOT || 'FALSE';
-const author = process.env.STICKER_AUTHOR ||'⚜️DRAGON-AI⚜️';
-const packname = process.env.STICKER_PACKNAME || 'DRAGON-AI🗿';
+const author = process.env.STICKER_AUTHOR ||'⚜️ Mr-heisme 🎭 ⚜️';
+const packname = process.env.STICKER_PACKNAME || 'Mr-heisme 🗿';
 const antitag = process.env.ANTITAG || 'TRUE';
-const dev = process.env.DEV || '254702713600';
+const dev = process.env.DEV || '255744528835';
 
 const menu = process.env.MENU_TYPE || 'VIDEO';
 const DevRaven = dev.split(",");
 const badwordkick = process.env.BAD_WORD_KICK || 'FALSE';
 const bad = process.env.BAD_WORD || 'fuck';
-const autoread = process.env.AUTOREAD || 'FALSE';
-const antidel = process.env.ANTIDELETE || 'TRUE';
+const autoread = process.env.AUTOREAD || 'TRUE';
+const antidel = process.env.ANTIDELETE || 'FALSE';
 
-const admin = process.env.ADMIN_MSG || '𝗖𝗼𝗺𝗺𝗮𝗻𝗱 𝗿𝗲𝘀𝗲𝗿𝘃𝗲𝗱 𝗳𝗼𝗿 𝗔𝗱𝗺𝗶𝗻𝘀!';
+const admin = process.env.ADMIN_MSG || '𝗖𝗼𝗺𝗺𝗮𝗻𝗱 𝗿𝗲𝘀𝗲𝗿𝘃𝗲𝗱 𝗳𝗼𝗿 Mr-heisme 🎭!';
 const group = process.env.GROUP_ONLY_MSG || '𝗖𝗼𝗺𝗺𝗮𝗻𝗱 𝗺𝗲𝗮𝗻𝘁 𝗳𝗼𝗿 𝗚𝗿𝗼𝘂𝗽𝘀!';
 const botAdmin = process.env.BOT_ADMIN_MSG || '𝗜 𝗻𝗲𝗲𝗱 𝗔𝗱𝗺𝗶𝗻 𝗽𝗿𝗲𝘃𝗶𝗹𝗲𝗱𝗴𝗲𝘀!';
-const NotOwner = process.env.NOT_OWNER_MSG || '𝗖𝗼𝗺𝗺𝗮𝗻𝗱 𝗺𝗲𝗮𝗻𝘁 𝗳𝗼𝗿 𝘁𝗵𝗲 𝗼𝘄𝗻𝗲𝗿!';
+const NotOwner = process.env.NOT_OWNER_MSG || '𝗖𝗼𝗺𝗺𝗮𝗻𝗱 𝗺𝗲𝗮𝗻𝘁 𝗳𝗼𝗿 𝘁𝗵𝗲 Mr-heisme 🎭!';
 
 const wapresence = process.env.WA_PRESENCE || 'recording';
 const antilink = process.env.ANTILINK || 'TRUE';
-const mycode = process.env.CODE || '254';
+const mycode = process.env.CODE || '255';
 const antiforeign = process.env.ANTIFOREIGN || 'FALSE';
 const port = process.env.PORT || 10000;
 const antilinkall = process.env.ANTILINK_ALL || 'TRUE';
